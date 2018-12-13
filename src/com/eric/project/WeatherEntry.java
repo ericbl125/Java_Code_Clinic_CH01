023a811c5d2c747a1d2102bbd509e0d81f39b59b
ericbl125 <ericblarson125@gmail.com>
2018-12-12 16:03:51 -0800

    Changed data types from String to Double

--- src/com/eric/project/WeatherEntry.java
+++ src/com/eric/project/WeatherEntry.java
@@ -8,88 +8,92 @@ public class WeatherEntry {
     // date       time    	Air_Temp	Barometric_Press	Dew_Point	Relative_Humidity	Wind_Dir	Wind_Gust	Wind_Speed
     // These are all the headers from the file.  Will only need to keep a handful for calculating the barometric pressure coefficient
 
-    private LocalDateTime ldt;
-    private String airTemp;
-    private String baroPress;
-    private String dew;
-    private String humidity;
-    private String windDir;
-    private String windGust;
-    private String windSpeed;
+    private LocalDateTime dateTime;
+    private Double airTemp;
+    private Double baroPress;
+    private Double dew;
+    private Double humidity;
+    private Double windDir;
+    private Double windGust;
+    private Double windSpeed;
 
     private DateTimeFormatter pattern =  DateTimeFormatter.ofPattern("yyyy_MM_dd HH:mm:ss");
 
     public WeatherEntry(String[] input) {
         int i = 0;
-        this.ldt = LocalDateTime.parse(input[i++], pattern);
-        this.airTemp = input[i++];
-        this.baroPress = input[i++];
-        this.dew = input[i++];
-        this.humidity = input[i++];
-        this.windDir= input[i++];
-        this.windGust = input[i++];
-        this.windSpeed = input[i];
+        this.dateTime = LocalDateTime.parse(input[i++], pattern);
+        this.airTemp = Double.valueOf(input[i++]);
+        this.baroPress = Double.valueOf(input[i++]);
+        this.dew = Double.valueOf(input[i++]);
+        this.humidity = Double.valueOf(input[i++]);
+        this.windDir= Double.valueOf(input[i++]);
+        this.windGust = Double.valueOf(input[i++]);
+        this.windSpeed = Double.valueOf(input[i++]);
     }
 
-    public LocalDateTime getDateTime() { return ldt; }
+    public LocalDateTime getDateTime() { return dateTime; }
 
-    public void setDateTime(LocalDateTime date) {
-        this.ldt = date;
-    }
-
-    public String getAirTemp() {
+    public Double getAirTemp() {
         return airTemp;
     }
 
-    public void setAirTemp(String airTemp) {
+    public void setAirTemp(Double airTemp) {
         this.airTemp = airTemp;
     }
 
-    public String getBaroPress() {
+    public Double getBaroPress() {
         return baroPress;
     }
 
-    public void setBaroPress(String baroPress) {
+    public void setBaroPress(Double baroPress) {
         this.baroPress = baroPress;
     }
 
-    public String getDew() {
+    public Double getDew() {
         return dew;
     }
 
-    public void setDew(String dew) {
+    public void setDew(Double dew) {
         this.dew = dew;
     }
 
-    public String getHumidity() {
+    public Double getHumidity() {
         return humidity;
     }
 
-    public void setHumidity(String humidity) {
+    public void setHumidity(Double humidity) {
         this.humidity = humidity;
     }
 
-    public String getWindDir() {
+    public Double getWindDir() {
         return windDir;
     }
 
-    public void setWindDir(String windDir) {
+    public void setWindDir(Double windDir) {
         this.windDir = windDir;
     }
 
-    public String getWindGust() {
+    public Double getWindGust() {
         return windGust;
     }
 
-    public void setWindGust(String windGust) {
+    public void setWindGust(Double windGust) {
         this.windGust = windGust;
     }
 
-    public String getWindSpeed() {
+    public Double getWindSpeed() {
         return windSpeed;
     }
 
-    public void setWindSpeed(String windSpeed) {
+    public void setWindSpeed(Double windSpeed) {
         this.windSpeed = windSpeed;
+    }
+
+    public DateTimeFormatter getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(DateTimeFormatter pattern) {
+        this.pattern = pattern;
     }
 }
